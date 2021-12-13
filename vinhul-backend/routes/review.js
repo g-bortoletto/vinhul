c32fd5447d5f20ebbc6ee495b42cfeed9da43b0c
Guilherme Bortoletto <guilherme.mbortoletto@gmail.com>
2021-12-13 17:40:58 -0300

    Add wine and review routers

---
+++ vinhul-backend/routes/review.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const db = require("../db");
+const router = express.Router();
+
+router.post('/createreview', (request, response) => {});
+
+router.get('/getreview', (request, response) => {});
+
+router.put('/updatereview', (request, response) => {});
+
+router.delete('/deletereview', (request, response) => {});
+
+module.exports = router;
---
+++ vinhul-backend/routes/wine.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const db = require("../db");
+const router = express.Router();
+
+router.post('/createwine', (request, response) => {});
+
+router.get('/getwine', (request, response) => {});
+
+router.put('/updatewine', (request, response) => {});
+
+router.delete('/deletewine', (request, response) => {});
+
+module.exports = router;
