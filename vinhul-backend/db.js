dbffe9625f5ae76ee490107e4dfa17c61abebd5e
Guilherme Bortoletto <guilherme.mbortoletto@gmail.com>
2021-12-13 19:14:00 -0300

    Add image to wine schema

--- vinhul-backend/db.js
+++ vinhul-backend/db.js
@@ -2,7 +2,7 @@ const mongooseConnection = require("mongoose");
 mongooseConnection
   .connect("mongodb://127.0.0.1:27017/vinhul", { useNewUrlParser: true })
   .then(console.log("[MESSAGE] Database connected..."))
-  .catch((error) => { console.error("[ERROR] " + error)});
+  .catch((error) => { console.error("[ERROR] " + error); });
 
 const userSchema = new mongooseConnection.Schema({
   email: String,
@@ -16,12 +16,20 @@ const wineSchema = new mongooseConnection.Schema({
   origin: String,
   type: String,
   grapeType: String,
-  foodHarmony: String
+  foodHarmony: String,
+  image: {
+    data: Buffer,
+    contentType: String
+  }
+}, {
+  collection: "wine"
 });
 
 const reviewSchema = new mongooseConnection.Schema({
   score: Number,
   message: String
+}, {
+  collection: "review"
 });
 
 module.exports = {
