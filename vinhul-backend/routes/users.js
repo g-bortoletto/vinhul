90df426a2684f04ce4a29d977428ce2c7f0647f6
Guilherme Bortoletto <guilherme.mbortoletto@gmail.com>
2021-12-13 20:14:06 -0300

    Improve account deletion

--- vinhul-backend/routes/users.js
+++ vinhul-backend/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const res     = require('express/lib/response');
-const router  = express.Router();
+const res = require('express/lib/response');
+const router = express.Router();
 
 router.post("/createaccount", (request, response) => {
   const db = require("../db");
@@ -9,7 +9,7 @@ router.post("/createaccount", (request, response) => {
     email: request.body.email,
     password: request.body.password,
     name: request.body.name
-  }
+  };
 
   UserModel.countDocuments({ email: newObj.email }, ((error, count) => {
     if (error) {
@@ -105,24 +105,21 @@ router.put('/updateaccount', (request, response) => {
   });
 });
 
-router.delete("/deleteaccount/", (req, res) => {
+router.delete("/deleteaccount/", (request, response) => {
   const db = require("../db");
+
   const UserModel = db.mongooseModule.model("user", db.userSchema, "user");
-  var email = req.query.email;
-  var response = "[MESSAGE] DELETE request to erase account.\n";
-  UserModel.findOneAndRemove({ email: email }, (error, _, __) => {
+  var email = request.query.email;
+
+  UserModel.findOneAndRemove({ email: email }, (error, document) => {
     if (error) {
-      response += "[ERROR] " + error;
-      res.send({ result: "ERROR" });
+      response.send({ result: "ERROR", message: error });
     } else if (document == null) {
       // If can't find a document with this email, return error
-      response += "[ERROR] User not found.";
-      res.send({ result: "ERROR" });
+      response.send({ result: "ERROR", message: "Account does not exist." });
     } else {
-      response += "[MESSAGE] Account erased.";
-      res.send({ result: "OK" });
+      response.send({ result: "OK", document: document });
     }
-    console.log(response);
   });
 });
 
